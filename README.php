8aa536f6f96d978cbb47c0a5393cd9416f360f2d
grevenilvec <chx3.cn@gmail.com>
2013-01-03 18:59:36 -0600

    jh

---
+++ README.php
@@ -0,0 +1,9 @@
+﻿README
+Sistema para calculo de Nomina y Comisiones
+
+Catalogos:
++PROGRAMAS (Web, Certificados, Etc)
++CONCEPTOS (Transportacion, Impuestos, etc.)
++AGENTES (Nombre, Fecha de ingreso, Sueldo, Estado (Activo, Inactivo)
++RESERVACIONES (Nombre del cliente, Fecha de pago, No. de reserva, PROGRAMA, CONCEPTO, Importe cobrado)
+
